04be743df8ee375a515d4be133b27c2260941229
aleyamayab <kingdom_ale@hotmail.com>
2024-03-11 03:39:41 -0600

    add

---
+++ my-app/src/main/java/myapp/Helloword/HelloWorldController.java
@@ -0,0 +1,11 @@
+package myapp.Helloword;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String helloworld() {
+        return "Hello World";
+    }
+}
