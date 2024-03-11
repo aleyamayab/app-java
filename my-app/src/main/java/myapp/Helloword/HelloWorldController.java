b7088a576cbd8c5a0e2e84abdadd8d783208067e
Alejandro Amaya <100615826+aleyamayab@users.noreply.github.com>
2024-03-11 16:29:14 -0600

    Update HelloWorldController.java
    correcto

--- my-app/src/main/java/myapp/Helloword/HelloWorldController.java
+++ my-app/src/main/java/myapp/Helloword/HelloWorldController.java
@@ -4,13 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
     @RequestMapping("/")
     public String helloworld() {
-        // Violación: No se cierra el bloque correctamente
-        if (true) {
-            System.out.println("Hello World";
-        }
         return "Hello World";
     }
 }
