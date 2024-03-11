ba172362c02809cebfe22afae07a69e90a2ac835
Alejandro Amaya <100615826+aleyamayab@users.noreply.github.com>
2024-03-11 16:55:57 -0600

    Update HelloWorldController.java
    correcto

--- my-app/src/main/java/myapp/Helloword/HelloWorldController.java
+++ my-app/src/main/java/myapp/Helloword/HelloWorldController.java
@@ -2,12 +2,10 @@ package myapp.Helloword;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloWorldController {
     @RequestMapping("/")
     public String helloworld() {
-        String password = "mysecretpassword";
-        // Aquí introducimos un problema de seguridad al imprimir la contraseña
-        System.out.println("Password: " + password);
         return "Hello World";
     }
 }
