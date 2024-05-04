2b570e708373747cd8209ccab77dcc509932bf79
aleyamayab <kingdom_ale@hotmail.com>
2024-05-03 22:23:53 -0600

    add

--- helloworld-java/src/test/java/myapp/Helloworld/HelloworldApplicationTests.java
+++ helloworld-java/src/test/java/myapp/Helloworld/HelloworldApplicationTests.java
@@ -1,10 +1,10 @@
-package myapp.Helloword;
+package myapp.Helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HellowordApplicationTests {
+class HelloworldApplicationTests {
 
 	@Test
 	void contextLoads() {
