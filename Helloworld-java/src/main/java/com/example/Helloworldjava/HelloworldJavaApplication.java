94561bed38ba2ab5404222f5b7ce0d1182a95202
aleyamayab <kingdom_ale@hotmail.com>
2024-05-03 22:47:26 -0600

    add

---
+++ Helloworld-java/src/main/java/com/example/Helloworldjava/HelloworldJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Helloworldjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldJavaApplication.class, args);
+	}
+
+}
