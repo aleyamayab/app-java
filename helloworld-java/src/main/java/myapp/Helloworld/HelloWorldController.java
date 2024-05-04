10a689b87fac75f4fa9030b0fc305337ba816d4e
aleyamayab <kingdom_ale@hotmail.com>
2024-05-03 22:07:19 -0600

    add wilmer java

---
+++ helloworld-java/src/main/java/myapp/Helloworld/HelloWorldController.java
@@ -0,0 +1,11 @@
+package myapp.Helloworld;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String helloworld() {
+        return "Hello World Wilmer";
+    }
+}
